5cf9d37ba16daae3c021182fff30f15162f563b1
Derek Heidorn <DerekHeidorn@sierrasystems.com>
2018-09-24 15:18:53 -0700

    Update manage file

--- manage.py
+++ manage.py
@@ -1,14 +1,29 @@
-from flask_script import Manager
-from web import createApplication
+from flask_script import Server, Manager
+from web.app import createApplication
 
-app = createApplication()
-manager = Manager(app)
+# app = createApplication()
+# manager = Manager(app)
+
+manager = Manager(createApplication)
+# manager.add_command("runserver", Server(host="0.0.0.0", port=9000))
 
 @manager.command
 def runserver():
-    # ptvsd.enable_attach(secret="my_secret", address=('0.0.0.0', 3000))
-    print('ptvsd is started')
-    options = {'use_debugger':True, 'threaded':False, 'use_reloader':True}
-    app.run(debug=False, host="0.0.0.0", port=5000, **options)
+    Server(host="127.0.0.1", port=9000)
+
+#     # ptvsd.enable_attach(secret="my_secret", address=('0.0.0.0', 3000))
 
 
+
+#     # print('ptvsd is started')
+#     # options = {'use_debugger':True, 'threaded':False, 'use_reloader':True}
+#     # manager.run(debug=False, host="0.0.0.0", port=5000, **options)
+#     manager.run()
+
+
+@manager.command
+def hello():
+    print("hello")
+
+# if __name__ == "__main__":
+#     manager.run()
