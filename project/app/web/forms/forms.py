a00cb6759ff7f7303e419b45db35fb42ccf8acaa
Derek Heidorn <DerekHeidorn@sierrasystems.com>
2018-11-06 08:14:10 -0800

    changes

--- project/app/web/forms/forms.py
+++ project/app/web/forms/forms.py
@@ -1,10 +1,18 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, ValidationError
 
 class UsernamePasswordForm(Form):
     username = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+
+    """
+    Validates the form by calling `validate` on each field, passing any
+    extra `Form.validate_<fieldname>` validators to the field validator.
+    """
+    def validate_username(self, field):
+        if len(field.data) > 2:
+            raise ValidationError('Name must be less than 2 characters')
 
 class SignupForm(Form):
     username = StringField('Email', validators=[DataRequired(), Email()])
