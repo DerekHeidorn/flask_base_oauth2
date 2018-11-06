32b0d82fe20d79c7df0888f64edfc4df01654cf3
Derek Heidorn <DerekHeidorn@sierrasystems.com>
2018-11-06 11:53:35 -0800

    Code Cleanup

--- project/app/models/codetables/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["CtUserStatuses", "CtUserTypes", "CtBatchJobCodes", "CtBatchJobStatuses"]
-
-
-from project.app.models.codetables.users import CtUserStatuses,CtUserTypes
-from project.app.models.codetables.batch import CtBatchJobCodes, CtBatchJobStatuses
--- project/app/services/codetablesService.py
+++
@@ -1,8 +0,0 @@
-
-from project.app.persist import codetableDao
-
-def getCodeTable(codetableName):
-
-    codetableData = codetableDao.getCodeTable(codetableName)
-
-    return codetableData
