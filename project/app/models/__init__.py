dbb54c9aee958ec9609e5234abf410736066b5e2
Derek Heidorn <DerekHeidorn@sierrasystems.com>
2018-11-06 14:34:47 -0800

    code cleanup

--- project/app/models/__init__.py
+++ project/app/models/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["Config", "SecurityAuthority", "SecurityGroup", "User"]
+__all__ = ["Config", "SecurityAuthority", "SecurityGroup", "User", "OAuth2Token", "OAuth2AuthorizationCode"]
 
 from project.app.models.common import Config
 from project.app.models.security import SecurityAuthority, SecurityGroup
 from project.app.models.user import User
+from project.app.models.oauth2 import OAuth2Token, OAuth2AuthorizationCode
