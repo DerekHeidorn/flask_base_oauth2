870b020acaf2f326dce3883a9cf2402f34bbe4a3
Derek Heidorn <DerekHeidorn@sierrasystems.com>
2018-11-07 09:47:00 -0800

    updating refresh database script

--- database/refresh_database.py
+++ database/refresh_database.py
@@ -5,11 +5,11 @@ def refreshDatabase():
     print("refreshDatabase...")
     databaseUri = 'postgresql://postgres:P$F$xs+n?5+Ug3AU5PTe3q@localhost/postgres'
 
-    files = ['database_scripts/wipe_database.sql'
-             , 'database_scripts/tables.sql'
-             , 'database_scripts/data_codetables.sql'
-             , 'database_scripts/test_data/TB_CONFIG.sql'
-             , 'database_scripts/test_data/TB_USER_data.sql']
+    files = ['scripts/wipe_database.sql'
+             , 'scripts/tables.sql'
+             , 'scripts/data_codetables.sql'
+             , 'scripts/test_data/TB_CONFIG.sql'
+             , 'scripts/test_data/TB_USER_data.sql']
     for f in files:
         psqlCmd = ["psql", "-f", f, "-a", databaseUri]
         process = Popen(psqlCmd, stdout=PIPE)
