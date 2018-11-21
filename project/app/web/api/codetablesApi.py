4f18b7e923511357e62e574eaae7da984dfcb467
Derek Heidorn <DerekHeidorn@sierrasystems.com>
2018-11-21 14:16:05 -0800

    Code cleanup

--- project/app/web/api/codetablesApi.py
+++ project/app/web/api/codetablesApi.py
@@ -26,13 +26,11 @@ def codetable_by_name(codetable_name):
         cached_codetable = _codetable_cache.get(codetable_name)
 
         if cached_codetable is not None:
-            print("\n*** CachedcodeTable: " + str(cached_codetable))
             return jsonify(cached_codetable)
 
         else:
             codetable_data = codetablesService.get_code_table(allowed_codetable)
             data = CodeTableSchema.dump(codetable_data, many=True)
-            print("codetableData=" + str(data))
             if data:
                 _codetable_cache.add(codetable_name, data)
                 resp = serializeUtils.generate_response_wrapper(data)
