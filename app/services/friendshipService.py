f6f7e2a18eb70056dc51fc05746cf79f924a7c98
Derek Heidorn <DerekHeidorn@sierrasystems.com>
2018-12-04 11:34:36 -0800

    Updating friendship logic

--- app/services/friendshipService.py
+++ app/services/friendshipService.py
@@ -1,5 +1,6 @@
 
 from app.persist import baseDao, userDao
+from app import core
 
 
 def add_pending_friendship(user_uuid, friend_user_uuid):
@@ -12,8 +13,20 @@ def add_pending_friendship(user_uuid, friend_user_uuid):
         userDao.add_pending_friendship(user.user_id, friend_user.user_id, session)
         session.commit()
 
-    friends = get_friends_by_user_id(user.user_id)
-    return friends
+    return friend_user
+
+
+def update_friendship_to_accepted(user_uuid, friend_user_uuid):
+    session = baseDao.get_session()
+
+    user = userDao.get_user_by_uuid(user_uuid, session)
+    friend_user = userDao.get_user_by_uuid(friend_user_uuid, session)
+
+    if user is not None and friend_user is not None:
+        userDao.update_friendship_to_accepted(user.user_id, friend_user.user_id, session)
+        session.commit()
+
+    return friend_user
 
 
 def remove_pending_friendship(user_uuid, friend_user_uuid):
@@ -26,8 +39,7 @@ def remove_pending_friendship(user_uuid, friend_user_uuid):
         userDao.remove_pending_friendship(user.user_id, friend_user.user_id, session)
         session.commit()
 
-    friends = get_friends_by_user_id(user.user_id)
-    return friends
+    return friend_user
 
 
 def remove_accepted_friendship(user_uuid, friend_user_uuid):
@@ -40,38 +52,54 @@ def remove_accepted_friendship(user_uuid, friend_user_uuid):
         userDao.remove_accepted_friendship(user.user_id, friend_user.user_id, session)
         session.commit()
 
-    friends = get_friends_by_user_id(user.user_id)
-    return friends
+    return friend_user
 
 
 def get_friends_by_user_id(user_id):
     session = baseDao.get_session()
 
     friendships = userDao.get_friendships_by_user_id(user_id, session)
+    pending_friend_request_ids = []
     pending_friend_ids = []
     accepted_friend_ids = []
 
     for friendship in friendships:
-        if friendship.user_id != user_id:
-            if friendship.status_cd == 'A':
-                accepted_friend_ids.append(friendship.user_id)
-            elif friendship.status_cd == 'P':
+        if friendship.status_cd == 'P':  # Pending friend request
+            if friendship.user_id == user_id:  # user initiated request
+                pending_friend_request_ids.append(friendship.friend_user_id)
+            elif friendship.friend_user_id == user_id:  # user needs to accept these users to be friends
                 pending_friend_ids.append(friendship.user_id)
-        if friendship.friend_user_id != user_id:
-            if friendship.status_cd == 'A':
-                accepted_friend_ids.append(friendship.friend_user_id)
-            elif friendship.status_cd == 'P':
-                pending_friend_ids.append(friendship.friend_user_id)
 
+        elif friendship.status_cd == 'A':
+
+            if friendship.user_id != user_id:
+                accepted_friend_ids.append(friendship.user_id)
+            elif friendship.friend_user_id != user_id:
+                accepted_friend_ids.append(friendship.friend_user_id)
+
+    core.logger.debug("pending_friend_request_ids=" + str(pending_friend_request_ids))
+    core.logger.debug("pending_friend_ids=" + str(pending_friend_ids))
+    core.logger.debug("accepted_friend_ids=" + str(accepted_friend_ids))
+
+    pending_friend_requests = list()
     pending_friends = list()
     accepted_friends = list()
+
+    if len(pending_friend_request_ids) > 0:
+        pending_user_requests = userDao.get_users_by_ids(pending_friend_request_ids, session)
+        for u in pending_user_requests:
+            pending_friend_requests.append(u)
+
     if len(pending_friend_ids) > 0:
         pending_users = userDao.get_users_by_ids(pending_friend_ids, session)
         for u in pending_users:
             pending_friends.append(u)
+
     if len(accepted_friend_ids) > 0:
         accepted_users = userDao.get_users_by_ids(accepted_friend_ids, session)
         for u in accepted_users:
             accepted_friends.append(u)
 
-    return {"pending_friends": pending_friends, "accepted_friends": accepted_friends}
+    return {"pending_friend_requests": pending_friend_requests,
+            "pending_friends": pending_friends,
+            "accepted_friends": accepted_friends}
--- app/web/api/friendshipApi.py
+++ app/web/api/friendshipApi.py
@@ -21,15 +21,21 @@ def get_my_friends():
         if current_user:
             friends = friendshipService.get_friends_by_user_id(current_user.user_id)
 
+            pending_requests_list = []
             pending_list = []
             accepted_list = []
+            for u in friends["pending_friend_requests"]:
+                pending_requests_list.append(PublicUserProfileSchema().dump(u))
+
             for u in friends["pending_friends"]:
                 pending_list.append(PublicUserProfileSchema().dump(u))
 
             for u in friends["accepted_friends"]:
                 accepted_list.append(PublicUserProfileSchema().dump(u))
 
-            data = {"pending_friends": pending_list, "accepted_friends": accepted_list}
+            data = {"pending_friend_requests": pending_requests_list,
+                    "pending_friends": pending_list,
+                    "accepted_friends": accepted_list}
 
             resp = apiUtils.generate_response_wrapper(data)
             return jsonify(resp)
@@ -41,7 +47,7 @@ def get_my_friends():
 
 
 @api.route('/api/v1.0/my/friend/user/<user_uuid>/<user_digest>', methods=['post'])
-@oauth2.require_oauth('GRP_ACCESS')
+@oauth2.require_oauth('CUST_ACCESS')
 def friend_user(user_uuid, user_digest):
 
     if current_token is not None and current_token.user is not None:
@@ -50,14 +56,9 @@ def friend_user(user_uuid, user_digest):
         digest_to_compare = sha256.hexdigest(user_uuid)
 
         if digest_to_compare == user_digest:
-            friends = friendshipService.add_pending_friendship(my_user_uuid, user_uuid)
+            pending_friend = friendshipService.add_pending_friendship(my_user_uuid, user_uuid)
 
-            friend_list = []
-            if friends is not None:
-                for u in friends:
-                    friend_list.append(PublicUserProfileSchema().dump(u))
-
-            resp = apiUtils.generate_response_wrapper(friend_list)
+            resp = apiUtils.generate_response_wrapper(PublicUserProfileSchema().dump(pending_friend))
             resp = apiUtils.add_global_success_msg(resp, "Requesting friendship sent")
 
             return jsonify(resp), 201
@@ -67,8 +68,50 @@ def friend_user(user_uuid, user_digest):
         abort(403)
 
 
+@api.route('/api/v1.0/my/friend/accept/user/<user_uuid>/<user_digest>', methods=['put'])
+@oauth2.require_oauth('CUST_ACCESS')
+def accept_friend_request(user_uuid, user_digest):
+
+    if current_token is not None and current_token.user is not None:
+        my_user_uuid = current_token.user.user_uuid
+
+        digest_to_compare = sha256.hexdigest(user_uuid)
+
+        if digest_to_compare == user_digest:
+            accepted_friend = friendshipService.update_friendship_to_accepted(my_user_uuid, user_uuid)
+
+            resp = apiUtils.generate_response_wrapper(PublicUserProfileSchema().dump(accepted_friend))
+            resp = apiUtils.add_global_success_msg(resp, "Accepted friend")
+            return jsonify(resp), 200
+        else:
+            abort(403)
+    else:
+        abort(403)
+
+
+@api.route('/api/v1.0/my/friend/deny/user/<user_uuid>/<user_digest>', methods=['put'])
+@oauth2.require_oauth('CUST_ACCESS')
+def deny_friend_request(user_uuid, user_digest):
+
+    if current_token is not None and current_token.user is not None:
+        my_user_uuid = current_token.user.user_uuid
+
+        digest_to_compare = sha256.hexdigest(user_uuid)
+
+        if digest_to_compare == user_digest:
+            removed_friend = friendshipService.remove_pending_friendship(my_user_uuid, user_uuid)
+
+            resp = apiUtils.generate_response_wrapper(PublicUserProfileSchema().dump(removed_friend))
+            resp = apiUtils.add_global_success_msg(resp, "Denied friend request")
+            return jsonify(resp), 200
+        else:
+            abort(403)
+    else:
+        abort(403)
+
+
 @api.route('/api/v1.0/my/unfriend/user/<user_uuid>/<user_digest>', methods=['put'])
-@oauth2.require_oauth('GRP_ACCESS')
+@oauth2.require_oauth('CUST_ACCESS')
 def unfriend_user(user_uuid, user_digest):
 
     if current_token is not None and current_token.user is not None:
@@ -77,14 +120,9 @@ def unfriend_user(user_uuid, user_digest):
         digest_to_compare = sha256.hexdigest(user_uuid)
 
         if digest_to_compare == user_digest:
-            friends = friendshipService.remove_accepted_friendship(my_user_uuid, user_uuid)
+            removed_friend = friendshipService.remove_accepted_friendship(my_user_uuid, user_uuid)
 
-            friend_list = []
-            if friends is not None:
-                for u in friends:
-                    friend_list.append(PublicUserProfileSchema().dump(u))
-
-            resp = apiUtils.generate_response_wrapper(friend_list)
+            resp = apiUtils.generate_response_wrapper(PublicUserProfileSchema().dump(removed_friend))
             resp = apiUtils.add_global_success_msg(resp, "Removed friend")
             return jsonify(resp), 200
         else:
